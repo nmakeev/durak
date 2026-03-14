05f101fbb481e8336bee32a503e810c531b911d4
Nick Makeev <n.s.makeev@gmail.com>
2026-03-14 13:53:21 +0300

    Add card model

---
+++ Assets/Game/Core/Cards/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Game.Core.Cards
+{
+    public readonly struct Card : IEquatable<Card>
+    {
+        public readonly Suit Suit;
+        public readonly Rank Rank;
+
+        public Card(Suit suit, Rank rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        bool IEquatable<Card>.Equals(Card other)
+        {
+            return Suit == other.Suit && Rank == other.Rank;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Suit, Rank);
+        }
+    }
+}
---
+++ Assets/Game/Core/Cards/Suit.cs
@@ -0,0 +1,10 @@
+namespace Game.Core.Cards
+{
+    public enum Suit : byte
+    {
+        Clubs = 0,
+        Diamonds = 1,
+        Hearts = 2,
+        Spades = 3
+    }
+}
